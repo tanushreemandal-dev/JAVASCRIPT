41973f9ce5c7a577e38f310b8347a08b77fd60da
tanushreemandal-dev <mandaltanishree@gmail.com>
2025-05-29 03:51:24 +0000

    first commit done

---
+++ 01_BASICS/test.js
@@ -0,0 +1,52 @@
+console.log("Hello, World!");
+console.log("This is a test file.");
+let acId="1234567890";
+const acc=123
+let t=9007
+console.table([acId,acc,t])
+let x1="Tanu"
+console.log(typeof x1)
+let num=Boolean(x1)
+console.log(typeof num)
+console.log(num)
+let x=3
+let neg=-x
+console.log(neg)
+
+let str1="Hi"
+let str2=" Hello"
+let str3=str1+str2
+console.log(str3)
+let y=100
+console.log(++y)
+console.log(y++)
+console.log(y)
+let arr=["Tanu", "Rani", "Soni"]
+console.log(arr[0])
+const n=[ "Rani", "Soni"]
+console.log(n[1])
+let myObj={
+    name :"hitesh",
+    age:67
+}
+console.log(myObj.age)
+
+let mail="abc"
+let mail1=mail
+mail1="tyu"
+console.log(mail)
+console.log(mail1)
+let obj={
+    name:"Tanu",
+    age:23
+}
+let obj1=obj
+obj1.name="Anu"
+console.log(obj.name)
+console.log(obj1.name)
+
+ const name1="hitesh"
+ let count=15
+ console.log(`my name is ${name1} and count is ${count}`)
+const name =new String("asd")
+console.log(name[0])
