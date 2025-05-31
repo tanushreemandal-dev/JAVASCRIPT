ebb475e9e4118cb3c7f963606f0910fef2cb8133
tanushreemandal-dev <mandaltanishree@gmail.com>
2025-05-31 09:52:43 +0530

    Create numbers.js

---
+++ 01_BASICS/numbers.js
@@ -0,0 +1,12 @@
+const n=400
+console.log(n)
+const x=new Number(400)
+console.log(x)
+console.log(x.toString().length)
+console.log(typeof x)
+console.log(x.toFixed(3))
+const y=new Number(456.896)
+console.log(y.toPrecision(2))
+
+const num=100000
+console.log(num.toLocaleString('en-IN'))
