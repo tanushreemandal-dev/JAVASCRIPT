5cbafd3dbc61be1a258aa4eb6a9fe9e965a1ad63
tanushreemandal-dev <mandaltanishree@gmail.com>
2025-05-31 09:53:27 +0530

    Create math.js

---
+++ 01_BASICS/math.js
@@ -0,0 +1,16 @@
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.7))
+console.log(Math.round(4.3))
+console.log(Math.sqrt(49))
+console.log(Math.pow(2,3))
+console.log(Math.min(1,2,3,4,5,6,0,-7,-1))
+console.log(Math.max(-1,-2,0))
+console.log(Math.sin(0))
+
+console.log((Math.random()*10)+1)
+console.log(Math.floor((Math.random()*10))+1)
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
