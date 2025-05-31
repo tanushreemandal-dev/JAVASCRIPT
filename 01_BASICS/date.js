23215519c45c3c1ac8c6b5c5129a3e7d76d481d0
tanushreemandal-dev <mandaltanishree@gmail.com>
2025-05-31 09:54:18 +0530

    Create date.js

---
+++ 01_BASICS/date.js
@@ -0,0 +1,10 @@
+let myDate=new Date()
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+console.log(typeof(myDate))
+
+let date= new Date("01-29-2025");
+console.log(date.getTime())
+let timeStamp=Date.now();
+console.log(timeStamp);
+console.log(Math.floor(timeStamp/1000))
