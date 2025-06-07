e4090f2876e174c75bf06726fc3df1c316bf1267
tanushreemandal-dev <mandaltanishree@gmail.com>
2025-06-07 12:32:17 +0000

    arrows & scope commit

---
+++ 03_BASICS/arrows.js
@@ -0,0 +1,22 @@
+// const user={
+//     username:"tan",
+//     price:788,
+//     welcomeMessage:function(){
+//         console.log(`Username is ${this.username} and price is ${this.price} `)
+//        //console.log(this)
+//     }
+// }
+
+//  user.welcomeMessage()
+//  user.username="gauri"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// const add=(num1,num2)=>{  //EXPLICIT RETURN
+//     return num1+num2
+// }
+// console.log(add(4,5)) 
+
+const add=(num1,num2)=>num1+num2  //IMPLICIT RETURN
+console.log(add(5,5)) 
--- 03_BASICS/scope.js
+++ 03_BASICS/scope.js
@@ -4,8 +4,19 @@ if(true){
     let a=300
     let b=1
     const c=9
-    console.log("Inner:",a)
+   // console.log("Inner:",a)
 }
-console.log(a)
+//console.log(a)
 // console.log(b)
 // console.log(c)
+
+function one(){
+    const username="tan"
+    function two(){
+        const age=12
+        console.log(username)
+    }
+   // console.log(age)
+    two()
+}
+one()
