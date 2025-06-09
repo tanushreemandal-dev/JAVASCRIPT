865e2ec836def83bbf776466e9fcfd99d2806ab5
tanushreemandal-dev <mandaltanishree@gmail.com>
2025-06-09 05:30:16 +0000

    loop commit

--- 04_BASICS/loops.js
+++ 04_BASICS/loops.js
@@ -1,10 +1,11 @@
-function mul(num){
-
-for(let j=0;j<=10;j++){
-    console.log(`${num} * ${j} = ${num*j}`)
+function mul(){
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=10;j++){
+    console.log(`${i} * ${j} = ${i*j}`)
 }
 }
-const res=mul(2)
+}
+const res=mul()
 console.log(res)
  
 
