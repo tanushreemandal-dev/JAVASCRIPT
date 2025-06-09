3635af13cead7968a2f0416288d337a11cc9d94b
tanushreemandal-dev <mandaltanishree@gmail.com>
2025-06-09 03:40:53 +0000

    scope commit

--- 03_BASICS/scope.js
+++ 03_BASICS/scope.js
@@ -1,26 +1,30 @@
 //var a=30
-let a=30
-if(true){
-    let a=300
-    let b=1
-    const c=9
-   // console.log("Inner:",a)
-}
-//console.log(a)
-// console.log(b)
-// console.log(c)
+// let a=30
+// if(true){
+//     let a=300
+//     let b=1
+//     const c=9
+//    // console.log("Inner:",a)
+// }
+// //console.log(a)
+// // console.log(b)
+// // console.log(c)
 
-function one(){
-    const username="tan"
-    function two(){
-        const age=12
-        console.log(username)
-    }
-   // console.log(age)
-    two()
-}
-one()
-//IIFE
-(function print(){
+// function one(){
+//     const username="tan"
+//     function two(){
+//         const age=12
+//         console.log(username)
+//     }
+//    // console.log(age)
+//     two()
+// }
+// one()
+// IIFE
+ (function print(){
     console.log("T")
-})()
+})();
+
+(()=>{
+    console.log("p")
+})();
