67bdd6edc5b0bf54fe0352619acd631281e3b334
tanushreemandal-dev <mandaltanishree@gmail.com>
2025-06-09 07:54:34 +0000

    loops commit

--- 04_BASICS/loops.js
+++ 04_BASICS/loops.js
@@ -32,10 +32,23 @@
 //     console.log(i)
 // }
 
-let i=0
-while(i<=5){
-    console.log(i)
-    i++
-}
+// let i=0
+// while(i<=5){
+//     console.log(i)
+//     i++
+// }
+
+// let i=1
+// do{
+//    console.log(i)
+// }while(i==0){
+//     console.log(i)
+//     i++
+// }
+ let i = 1;
+do {
+  console.log(i);
+} while (i == 0);
+
  
 
